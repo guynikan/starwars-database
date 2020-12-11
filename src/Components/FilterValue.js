31755dd591161fa15a36981f5c5405bda5005c28
Ed <jr-lol@live.com>
2020-12-11 18:07:47 -0300

    adicionada func bidirecional

--- src/Components/FilterValue.js
+++ src/Components/FilterValue.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 function FilterValue ({ filter, onFilterValueChange }) {
   function handleFilterValue ({ target }) {
-    onFilterValueChange(target.value)
+    onFilterValueChange(target.value, target.id)
   }
-  return <input type='number' value={filter} onChange={handleFilterValue} />
+  return (
+    <input
+      id='value'
+      type='number'
+      value={filter}
+      onChange={handleFilterValue}
+    />
+  )
 }
 
 export default FilterValue
