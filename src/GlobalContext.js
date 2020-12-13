881a923738628c33f6d400d79266048deb791d47
Ed <jr-lol@live.com>
2020-12-13 20:38:31 -0300

    criados estados e constante da paginação

--- src/GlobalContext.js
+++ src/GlobalContext.js
@@ -1,13 +1,42 @@
 import React from 'react'
 import useFetch from './Hooks/useFetch'
 
+const PER_PAGE = 10
+
 export const GlobalContext = React.createContext()
 
 export const GlobalStorage = ({ children }) => {
-  const { request, data, setData, loading, error } = useFetch()
+  const [currentPage, setCurrentPage] = React.useState(0)
+  const [pageCount, setPageCount] = React.useState(0)
+
+  const offset = currentPage * PER_PAGE
+
+  const {
+    requestRecursively,
+    getData,
+    data,
+    setData,
+    error,
+    loading
+  } = useFetch()
 
   return (
-    <GlobalContext.Provider value={{ request, data, setData, loading, error }}>
+    <GlobalContext.Provider
+      value={{
+        requestRecursively,
+        getData,
+        data,
+        setData,
+        error,
+        loading,
+        PER_PAGE,
+        offset,
+        currentPage,
+        setCurrentPage,
+        pageCount,
+        setPageCount
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   )
