2e4c188caa27bea4e18754dbdd522f23b795595f
Ed <jr-lol@live.com>
2020-12-13 20:39:07 -0300

    adicionada library react-paginate

--- src/Components/FilterableTable.js
+++ src/Components/FilterableTable.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import ReactPaginate from 'react-paginate'
+import '../Styled/App.css'
 import Filter from './Filter'
 import Table from './Table'
 import GlobalContext from '../GlobalContext'
@@ -8,7 +10,14 @@ import Pagination from '../Styled/Pagination'
 import Title from '../Styled/Title'
 
 function FilterableTable () {
-  const { request, data } = React.useContext(GlobalContext)
+  const {
+    requestRecursively,
+    setData,
+    offset,
+    setCurrentPage,
+    pageCount
+  } = React.useContext(GlobalContext)
+
   const [filter, setFilter] = React.useState({
     filters: {
       hasFilter: false,
@@ -26,34 +35,46 @@ function FilterableTable () {
   })
 
   React.useEffect(() => {
-    console.log('requisitou')
-    request('https://swapi-trybe.herokuapp.com/api/planets/')
-  }, [])
+    console.log('fez o request')
+    requestRecursively('https://swapi-trybe.herokuapp.com/api/planets/').then(
+      data => setData(data)
+    )
+  }, [offset])
 
-  function PreviousPage () {
-    request(`${data.previous}`)
-  }
+  // function PreviousPage () {
+  //   request(`${data.previous}`)
+  // }
 
-  function NextPage () {
-    request(`${data.next}`)
+  // function NextPage () {
+  //   request(`${data.next}`)
+  // }
+
+  function handlePageClick ({ selected: selectedPage }) {
+    debugger
+    setCurrentPage(selectedPage)
   }
 
   return (
-    data && (
-      <Container>
-        <Title>DEATH STAR 2.0</Title>
-        <Filter filter={filter} setFilter={setFilter} />
-        <Table planets={data} filter={filter} setFilter={setFilter} />
-        <Pagination>
-          <Button handleClick={PreviousPage} disabled={!data.previous}>
-            Previous
-          </Button>
-          <Button handleClick={NextPage} disabled={!data.next}>
-            Next
-          </Button>
-        </Pagination>
-      </Container>
-    )
+    <Container>
+      <Title>DEATH STAR 2.0</Title>
+      <Filter filter={filter} setFilter={setFilter} />
+      <Table filter={filter} setFilter={setFilter} />
+      <Pagination>
+        <ReactPaginate
+          previousLabel={'prev'}
+          nextLabel={'next'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageClick}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+        />
+      </Pagination>
+    </Container>
   )
 }
 
