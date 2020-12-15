24bf9973500473ecca3b523943f92ea57375911f
Ed <jr-lol@live.com>
2020-12-15 02:00:23 -0300

    aplicado estilo base aos componentes

--- src/Styled/StyledInput.js
+++ src/Styled/StyledInput.js
@@ -1,18 +1,6 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Input = styled.input``
-
-export const SearchInput = styled(Input)`
-  width: 100%;
-  padding: 5px 10px;
-  border: none;
-  border-radius: 5px;
-  margin: 10px 10px;
-  box-shadow: 2px 2px 2px #575757;
-`
-
-export const NumericInput = styled(Input)`
-  flex: 1;
+export default styled.input`
   padding: 5px 10px;
   border: none;
   border-radius: 5px;
